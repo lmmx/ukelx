683bc15722eb4debea13a47274c9dc8330824fb3
Louis Maddox <louismmx@gmail.com>
2024-07-05 17:36:48 +0100

    feat: more info

--- ukelx/jinja.py
+++ ukelx/jinja.py
@@ -5,3 +5,13 @@ __all__ = ["templates"]
 
 template_dir = Path(__file__).parent / "templates"
 templates = Jinja2Templates(directory=template_dir)
+
+# Custom filter for datetime formatting
+def datetime_filter(timestamp):
+    from datetime import datetime
+    if timestamp is None:
+        return "Not available"
+    return datetime.fromtimestamp(timestamp / 1000).strftime('%Y-%m-%d %H:%M:%S')
+
+# Add the custom filter to the Jinja2 environment
+templates.env.filters['datetime'] = datetime_filter
